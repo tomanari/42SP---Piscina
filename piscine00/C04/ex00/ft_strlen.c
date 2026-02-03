577bec0176f5eecc1d4121cdd2f90e02e9c022f9
tomanari <tomanari.lol@gmail.com>
2026-02-03 14:21:38 -0300

    Add C04

---
+++ piscine00/C04/ex00/ft_strlen.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlen.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mtomanar <mtomanar@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/02 19:04:15 by mtomanar          #+#    #+#             */
+/*   Updated: 2026/02/02 19:08:04 by mtomanar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int ft_strlen(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+#include <stdio.h>
+
+int main(void)
+{
+	printf("comprimero da string: %d", ft_strlen("Marcella"));
+	return 0;
+}
---
+++ piscine00/C04/ex01/ft_putstr.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mtomanar <mtomanar@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/02 19:09:37 by mtomanar          #+#    #+#             */
+/*   Updated: 2026/02/02 19:22:55 by mtomanar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void ft_putstr(char *str)
+{
+	while(*str !='\0')
+	{
+		ft_putchar(*str);
+		str++;
+	}
+}
+
+int main(void)
+{
+	ft_putstr("MArcella");
+	return 0;
+}

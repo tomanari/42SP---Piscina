9cf9128933203c91c52288540b601122930bf0ec
tomanari <tomanari.lol@gmail.com>
2026-02-05 09:48:48 -0300

    att

---
+++ piscine00/C04/ex03/ft_atoi.c
@@ -0,0 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mtomanar <mtomanar@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/05 09:07:15 by mtomanar          #+#    #+#             */
+/*   Updated: 2026/02/04 17:34:24 by mtomanar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+// #include <unistd.h>
+
+// void  ft_putchar(char c)
+// {
+//   write(1, &c, 1);
+// }
+
+// void  ft_putnbr(int nb)
+// {
+//   long int  n;
+
+//   n = nb;
+//   if (n < 0)
+//     ft_putchar('-');
+//     n *= -1;
+//   while (n > 9)
+//     ft_putnbr(n / 10);
+//   n = n % 10 + '0';
+//   ft_putchar(n);
+// }
+
+int  ft_atoi(char *str)
+{
+  int  res;
+  int  sign;
+
+  sign = 1;
+  res = 0;
+  while (*str == ' ' || *str >= 9 && *str <= 13)
+  {  
+    str++;
+  }
+  if (*str == '-' || *str == '+')
+  {
+    if(*str == '-')
+      sign = -1;
+    str++;
+  }
+  while (*str >= '0' && *str <= '9')
+  { 
+    res = res * 10 + (*str - '0');
+    str++;
+  }
+  return (res * sign);
+}
